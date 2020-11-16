783b6521ad094b2cc9f523293eda54d259ec5893
nbirdie <71641424+nbirdie@users.noreply.github.com>
2020-11-16 16:39:38 +0300

    Update ft_strtrim.c

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -11,6 +11,12 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+/*
+#1. The string to be trimmed.
+#2. The reference set of characters to trim
+Allocates (with malloc(3)) and returns a copy of ’s1’ with the characters specified in ’set’ removed
+from the beginning and the end of the string.
+*/
 
 char		*ft_check(char c, char const *s)
 {
