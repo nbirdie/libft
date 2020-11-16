20f6658dff0e44e689a0d9763f81f9092d77e466
nbirdie <71641424+nbirdie@users.noreply.github.com>
2020-11-16 16:38:51 +0300

    Update ft_strjoin.c

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -14,8 +14,7 @@
 /*
 #1. The prefix string.
 #2. The suffix string.
-Allocates (with malloc(3)) and returns a new
-string, which is the result of the concatenation
+Allocates (with malloc(3)) and returns a new string, which is the result of the concatenation
 of ’s1’ and ’s2’.
 */
 
