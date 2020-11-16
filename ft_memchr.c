4907efff5942c456ca6077f78defb98833700b86
nbirdie <71641424+nbirdie@users.noreply.github.com>
2020-11-16 17:18:30 +0300

    Update ft_memchr.c

--- ft_memchr.c
+++ ft_memchr.c
@@ -11,6 +11,11 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+/*
+memchr -- locate byte in byte string
+The memchr() function locates the first occurrence of c (converted to an unsigned char) in string s.
+The memchr() function returns a pointer to the byte located, or NULL if no such byte exists within n bytes.
+*/
 
 void				*ft_memchr(const void *s, int c, size_t n)
 {
