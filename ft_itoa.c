7abdc5b8b0f0386d7f9c03a4a0af1369ec5e477b
nbirdie <71641424+nbirdie@users.noreply.github.com>
2020-11-16 16:41:07 +0300

    Update ft_itoa.c

--- ft_itoa.c
+++ ft_itoa.c
@@ -11,6 +11,12 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+/*
+#1. the integer to convert.
+Allocates (with malloc(3)) and returns a string
+representing the integer received as an argument.
+Negative numbers must be handled.
+*/
 
 char		ft_int_len(int n)
 {
