acc9128801adfef9643d0ce024ec217a116c7e10
nbirdie <71641424+nbirdie@users.noreply.github.com>
2020-11-16 16:41:40 +0300

    Update ft_strmapi.c

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -11,6 +11,13 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+/*
+#1. The string on which to iterate.
+#2. The function to apply to each character.
+Applies the function ’f’ to each character of the
+string ’s’ to create a new string (with malloc(3))
+resulting from successive applications of ’f’.
+*/
 
 char				*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
