7bf8862d310abd645593f4bc6b2cdf491db9c0e1
nbirdie <71641424+nbirdie@users.noreply.github.com>
2020-11-16 15:57:47 +0300

    Update ft_memcpy.c

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -11,6 +11,12 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+/*
+memcpy -- copy memory area
+The memcpy() function copies n bytes from memory area src to memory area dst. If dst and src overlap, behavior is undefined.
+Applications in which dst and src might overlap should use memmove(3) instead.
+The memcpy() function returns the original value of dst.
+*/
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
@@ -20,8 +26,8 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	s1 = (char *)dst;
 	s2 = (char *)src;
 	if (dst == src)
-		return (dst);
+		return (dst);	//чтобы ускорить процесс проверяем равенство сначала
 	while (n--)
-		*s1++ = *s2++;
+		*s1++ = *s2++;	//иначе пока size не закончится копируем
 	return (dst);
 }
